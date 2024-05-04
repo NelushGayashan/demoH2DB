c6653c775f9e28e2f1614aac5c50ffa5915ea9b4
NGFXavier <nelushgayashan@gmail.com>
2024-05-04 22:29:52 +0530

    Initial commit

---
+++ src/main/java/com/example/demoH2DB/DemoH2DbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoH2DB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoH2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoH2DbApplication.class, args);
+	}
+
+}
